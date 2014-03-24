b3d8451b04834ed88df6e9ea3467174e1b849c2f
eckama11 <eckhardt.aman@uwlax.edu>
2014-03-24 11:20:09 -0500

    Create air_quality.sql
    file to add air_quality sql tables

---
+++ air_quality/air_quality.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE air_quality;
+USE air_quality;
+
+CREATE TABLE temperature(
+    id INT NOT NULL AUTO_INCREMENT,
+    temperature DECIMAL(3,2) NOT NULL,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE humidity(
+    id INT NOT NULL AUTO_INCREMENT,
+    humidity DECIMAL(3,2) NOT NULL,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE pressure(
+    id INT NOT NULL AUTO_INCREMENT,
+    pressure DECIMAL(3,2) NOT NULL,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE altitude(
+    id INT NOT NULL AUTO_INCREMENT,
+    altitude DECIMAL(3,2) NOT NULL,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE sensorGPS(
+    id INT NOT NULL AUTO_INCREMENT,
+    latitude DECIMAL(2,7) NOT NULL,
+    longitude DECIMAL(3,7) NOT NULL,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE sensorParticle(
+    id INT NOT NULL AUTO_INCREMENT,
+    timeInfo DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
