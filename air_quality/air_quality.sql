ed4aef7bacfb4296b48e6ab41b35da692d831350
eckama11 <eckhardt.aman@uwlax.edu>
2014-04-10 11:17:52 -0500

    Update air_quality.sql
    -modified User to have email.
    -created imp table to keep track of multiple imps for one user.
    -created sensors table while deleting other tables to create one place to keep all measurements based on time stamp date.

--- air_quality/air_quality.sql
+++ air_quality/air_quality.sql
@@ -3,46 +3,16 @@ DROP DATABASE IF EXISTS air_quality;
 CREATE DATABASE air_quality;
 USE air_quality;
 
-CREATE TABLE temperature(
+Create TABLE sensors(
     id INT NOT NULL AUTO_INCREMENT,
+    impId INT NOT NULL,
+    timeInfo DATETIME NOT NULL,
     temperature DECIMAL(3,2) NOT NULL,
-    timeInfo DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE humidity(
-    id INT NOT NULL AUTO_INCREMENT,
     humidity DECIMAL(3,2) NOT NULL,
-    timeInfo DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE pressure(
-    id INT NOT NULL AUTO_INCREMENT,
     pressure DECIMAL(3,2) NOT NULL,
-    timeInfo DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE altitude(
-    id INT NOT NULL AUTO_INCREMENT,
     altitude DECIMAL(3,2) NOT NULL,
-    timeInfo DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-
-CREATE TABLE sensorGPS(
-    id INT NOT NULL AUTO_INCREMENT,
     latitude DECIMAL(7,7) NOT NULL,
     longitude DECIMAL(7,7) NOT NULL,
-    timeInfo DATETIME NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE sensorParticle(
-    id INT NOT NULL AUTO_INCREMENT,
-    timeInfo DATETIME NOT NULL,
     particles DECIMAL(3,3) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -51,8 +21,16 @@ CREATE TABLE user(
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY (username)
+);
+
+CREATE TABLE imp(
+    id INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(userId) REFERENCES user(id)
 );
 
 CREATE TABLE loginSession(
